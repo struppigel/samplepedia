96e5fbe399ed6cc25cd2d9aa544b36bb15065c68
struppigel <struppigel@googlemail.com>
2025-12-25 10:45:48 +0100

    added course references to model, changed Tools to a tag list

--- sampledb/samples/admin.py
+++ sampledb/samples/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
-from .models import Sample
+from .models import Sample, CourseReference
+
+@admin.register(CourseReference)
+class CourseReferenceAdmin(admin.ModelAdmin):
+    list_display = ("course_name", "section", "video_title_short")
+    list_filter = ("course_name", "section")
+    search_fields = ("video_title",)
+    ordering = ("course_name", "section")
+    
+    def video_title_short(self, obj):
+        return obj.video_title[:50] + "..." if len(obj.video_title) > 50 else obj.video_title
+    video_title_short.short_description = "Video Title"
+
 
 @admin.register(Sample)
 class SampleAdmin(admin.ModelAdmin):
     list_display = ("sha256", "difficulty")
     list_filter = ("difficulty",)
     search_fields = ("sha256", "goal", "description")
+    filter_horizontal = ("course_references",)
--- sampledb/samples/models.py
+++ sampledb/samples/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 from taggit.managers import TaggableManager
+from taggit.models import TaggedItemBase
 from cloudinary.models import CloudinaryField
 
 
@@ -9,6 +11,40 @@ class Difficulty(models.TextChoices):
     MEDIUM = "medium", "Medium"
     ADVANCED = "advanced", "Advanced"
     EXPERT = "expert", "Expert"
+
+
+class CourseName(models.TextChoices):
+    BEGINNER = "beginner", "Beginner"
+    INTERMEDIATE = "intermediate", "Intermediate"
+
+
+class TaggedTools(TaggedItemBase):
+    content_object = models.ForeignKey('Sample', on_delete=models.CASCADE)
+
+
+class CourseReference(models.Model):
+    course_name = models.CharField(
+        max_length=20,
+        choices=CourseName.choices,
+        verbose_name="Course name"
+    )
+    
+    section = models.IntegerField(
+        verbose_name="Section",
+        validators=[MinValueValidator(0), MaxValueValidator(20)]
+    )
+    
+    video_title = models.CharField(
+        max_length=500,
+        verbose_name="Video title"
+    )
+    
+    class Meta:
+        ordering = ['course_name', 'section']
+        unique_together = ['course_name', 'section', 'video_title']
+    
+    def __str__(self):
+        return f"{self.get_course_name_display()} - Section {self.section}: {self.video_title[:50]}"
 
 
 class Sample(models.Model):
@@ -29,11 +65,6 @@ class Sample(models.Model):
         verbose_name="Description"
     )
 
-    tools = models.TextField(
-        blank=True,
-        verbose_name="Tools"
-    )
-
     goal = models.TextField(
         blank=True,
         verbose_name="Goal"
@@ -46,11 +77,13 @@ class Sample(models.Model):
         verbose_name="Difficulty level"
     )
     
-    tags = TaggableManager(blank=True)
+    tags = TaggableManager(blank=True, verbose_name="Tags", related_name='tagged_samples')
+    tools = TaggableManager(blank=True, verbose_name="Tools", related_name='tool_samples', through=TaggedTools)
     youtube_id = models.CharField(max_length=32, blank=True, verbose_name="YouTube ID")
     image = CloudinaryField('image', blank=True, null=True)
     like_count = models.IntegerField(default=0, verbose_name="Like count")
     favorited_by = models.ManyToManyField(User, related_name='favorite_samples', blank=True)
+    course_references = models.ManyToManyField(CourseReference, related_name='samples', blank=True, verbose_name="Course references")
     
     @property
     def favorite_count(self):
