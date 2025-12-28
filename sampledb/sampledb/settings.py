5dc0d660ead9c9e0680efd0890feb3a9dbd4c4e8
struppigel <struppigel@googlemail.com>
2025-12-28 08:52:14 +0100

    anymail support

--- sampledb/sampledb/settings.py
+++ sampledb/sampledb/settings.py
@@ -166,12 +166,20 @@ LOGIN_URL = '/accounts/login/'
 # For development, use console backend (prints emails to terminal)
 # For production, configure SMTP settings via environment variables
 EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='')
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
+# SMTP settings (if using SMTP backend)
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='')
+# HTTP with anymail
+ANYMAIL = {
+    "MAILGUN_API_KEY": config("MAILGUN_API_KEY", default=""),
+    "MAILGUN_SENDER_DOMAIN": config("MAILGUN_DOMAIN", default="")
+}
+EMAIL_TIMEOUT = 10
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
