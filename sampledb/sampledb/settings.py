c290536eb303797ede219739096c42364b33798d
struppigel <struppigel@googlemail.com>
2025-12-28 08:41:03 +0100

    added anymail

--- sampledb/sampledb/settings.py
+++ sampledb/sampledb/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'taggit',
     'samples',
+    "anymail",
 ]
 
 MIDDLEWARE = [
@@ -170,7 +171,7 @@ EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@samplepedia.com')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
