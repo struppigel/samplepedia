57ce48dad2a86bb4c436f03a3b7d22e6bbdb3ef0
struppigel <struppigel@googlemail.com>
2025-12-31 11:21:10 +0100

    redirect admin login to normal login

--- sampledb/sampledb/urls.py
+++ sampledb/sampledb/urls.py
@@ -3,8 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
+from samples.views import login_view
 
 urlpatterns = [
+    path("admin/login/", login_view, name="admin_login"),
     path("admin/", admin.site.urls),
     path("comments/", include('django_comments_xtd.urls')),
     path('favicon.ico', RedirectView.as_view(url='/static/myhegebatlogo_white.png', permanent=True)),
